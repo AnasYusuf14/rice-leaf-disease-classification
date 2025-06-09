7acb8e5d8cf9a01ed23ff6ed6ca8ce0ade503d27
anasyusuf <anas.yusuf200112@gmail.com>
2025-06-09 13:59:02 +0300

    Remove commented line in VGG16 model definition for clarity

--- rice-leaf-disease-classification/models/model_vgg16.py
+++ rice-leaf-disease-classification/models/model_vgg16.py
@@ -6,8 +6,7 @@ from tensorflow.keras.optimizers import Adam
 def build_vgg16_model(input_shape=(224, 224, 3), num_classes=4):
     base_model = VGG16(weights="imagenet", include_top=False, input_shape=input_shape)
     for layer in base_model.layers:
-        layer.trainable = False  # تجميد طبقات VGG16
-
+        layer.trainable = False 
     x = base_model.output
     x = Flatten()(x)
     x = Dense(256, activation='relu')(x)
